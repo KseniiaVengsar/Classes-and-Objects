28f80cee63469130c9278f201dfe0d5d4767431d
Kseniia Vengsar <122027680+KseniiaVengsar@users.noreply.github.com>
2023-03-20 12:29:53 +0000

    Update 1_Калькулятор_двух_чисел.cpp

--- 1_Калькулятор_двух_чисел/1_Калькулятор_двух_чисел.cpp
+++ 1_Калькулятор_двух_чисел/1_Калькулятор_двух_чисел.cpp
@@ -1,137 +1,121 @@
-﻿// 1_Калькулятор_двух_чисел.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// 2_Счётчик_class.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 
 #include <iostream>
 #include <cstdlib>
 #include <string>
+
 //описание класса
-class Calculator {
+class Counter {
+    //закрытые члены класса
 private:
     //поля
-    double num1;
-    double num2;
-    //методы.метод-аналог функции
+    int pole;
 public:
-    double add();
-    double multiply();
-    double subtract_1_2();
-    double subtract_2_1();
-    double divide_1_2();
-    double divide_2_1();
-    bool set_num1(double num1);
-    bool set_num2(double num2);
-    void SHOWALL();
+    void pluse_1();
+    void minus_1();
 
+    int present_value();
+
+    Counter() {
+        pole = 1;
+    }
+    Counter(int pole_) {
+        pole = pole_;
+    }
 };
-//double add() - метод должен возвращать результат сложения num1 и num2
-double Calculator::add() {
-    double SUM;
-    SUM = num1 + num2;
-    return SUM;
-}
-//double multiply() - метод должен возвращать результат перемножения num1 и num2
-double Calculator::multiply() {
-    double MULT;
-    MULT = num1 * num2;
-    return MULT;
-}
-//double subtract_1_2() - метод должен возвращать результат вычитания num2 из num1
-double Calculator::subtract_1_2() {
-    double SUB;
-    SUB = num2 - num1;
-    return SUB;
-}
-//double subtract_2_1() - метод должен возвращать результат вычитания num1 из num2
-double Calculator::subtract_2_1() {
-    double SUB2;
-    SUB2 = num1 - num2;
-    return SUB2;
-}
-//double divide_1_2() - метод должен возвращать результат деления num1 на num2
-double Calculator::divide_1_2() {
-    double DIV;
-    DIV = num1 / num2;
-    return DIV;
-}
-//double divide_2_1() - метод должен возвращать результат деления num2 на num1
-double Calculator::divide_2_1() {
-    double DIV2;
-    DIV2 = num2 / num1;
-    return DIV2;
-}
-//bool set_num1(double num1) - метод должен установить значение поля num1 равное значению аргумента num1 в случае, если значение аргумента не равно 0. Возвращает true, если установка произошла успешно, в обратном случае false
-//bool set_num2(double num2) - метод должен установить значение поля num2 равное значению аргумента num2 в случае, если значение аргумента не равно 0. Возвращает true, если установка произошла успешно, в обратном случае false
-bool Calculator::set_num1(double num1) {
-    
-    if ((this->num1 = num1) != 0)
-    {
-        return true;
 
-    }
-    else {
-        return false;
-    }
+void Counter::pluse_1() {
+    pole++;
+}
+//возможность уменьшить своё значение на 1
+void Counter::minus_1() {
+    pole--;
+}
+//возможность посмотреть своё текущее значение
+int Counter::present_value() {
+
+    return pole;
 }
 
-///////////////////////////////////////////////////////
-bool Calculator::set_num2(double num2) {
 
-    
-    if ((this->num2 = num2)!= 0)
-    {
-        return true;
-
-    }
-    else {
-        return false;
-    }
-}
-
-void Calculator::SHOWALL()
-{
-
-    std::cout << "num1 + num2 =" << " " << add() << std::endl;
-    std::cout << "num1*num2 =" << " " << multiply() << std::endl;
-    std::cout << "num2 - num1 =" << " " << subtract_1_2() << std::endl;
-    std::cout << "num1-num2 =" << " " << subtract_2_1() << std::endl;
-    std::cout << "num1 / num2 =" << " " << divide_1_2() << std::endl;
-    std::cout << "num2 / num1 =" << " " << divide_2_1() << std::endl;
-
-}
-//главная функция
 int main() {
     setlocale(LC_ALL, "RU");
-    //создание объекта
-    Calculator number;
+    Counter number;
+    Counter number2;
     //присваивание значений полям
+    std::cout << "Вы хотите указать начальное значение счётчика?" << "\n" << "Введите yes или no:" << std::endl;
+    std::string yes_no;
+    std::cin >> yes_no;
+    if (yes_no == "yes") {
+        std::cout << "Введите начальное значение счётчика:" << std::endl;
+        int m;
+        std::cin >> m;
+        number = Counter(m); //создание объекта
+        std::cout << "Введите команду ('+', '-', '=' или 'x'):";
+        std::string command_2;
+        std::cin >> command_2;
+        while (command_2 != "x")
+        {
+            std::cout << "Введите команду ('+', '-', '=' или 'x'):";
+            std::string command_2;
+            std::cin >> command_2;
+            if (command_2 == "+")
+            {
+                number.pluse_1();
+            }
+            else if (command_2 == "-")
+            {
+                number.minus_1();
+            }
+            else if (command_2 == "=")
+            {
+                std::cout << number.present_value() << std::endl;
+            }
 
-    //вызов метода
-    double c;
-    std::cout << "Введите num1:";
-    std::cin >> c;
-    while (number.set_num1(c) == false) {
-        std::cout << "Неверный ввод!" << std::endl;
-        std::cout << "Введите num1:";
-        std::cin >> c;
+            else if (command_2 == "x") {
+                std::cout << "До свидания!" << std::endl;
+                break;
+            }
+
+        }
     }
 
-    std::cout << "Введите num2:";
-    double z;
-    std::cin >> z;
-    while (number.set_num2(z) == false) {
-        std::cout << "Неверный ввод!" << std::endl;
-        std::cout << "Введите num2:";
-        std::cin >> z;
+    //////////////////////////////////////////////////////////////////////////////    
+
+    if (yes_no == "no") {
+        number2 = Counter();
+        std::cout << "Введите команду ('+', '-', '=' или 'x'):";
+        std::string command_5;
+        std::cin >> command_5;
+        while (command_5 != "x") {
+            std::cout << "Введите команду ('+', '-', '=' или 'x'):";
+            std::string command_5;
+            std::cin >> command_5;
+            if (command_5 == "+")
+            {
+                number.pluse_1();
+            }
+            else if (command_5 == "-")
+            {
+                number.minus_1();
+            }
+            else if (command_5 == "=")
+            {
+                std::cout << number.present_value() << std::endl;
+            }
+            else if (command_5 == "x") {
+                std::cout << "До свидания!" << std::endl;
+                break;
+            }
+
+        }
     }
 
- 
-
-    number.SHOWALL();
 
     return 0;
-
 }
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
